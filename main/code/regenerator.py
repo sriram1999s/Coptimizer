c9539d8679b95451fdecd4fe54f21a3f9deb71bc
khusheimeda <43701562+khusheimeda@users.noreply.github.com>
2021-02-27 22:31:18 +0530

    updated regenerator.py

--- main/code/regenerator.py
+++ main/code/regenerator.py
@@ -1,28 +1,29 @@
 
 #----------------------------------------------code generator ------------------------------------------------------
-
-def solve(i,n,l,output_prg):
-    if(i==n):
+def solve(start_index, end_index, parse_tree, output_prg):
+    space_list = ['int','float','void','return']
+    if(start_index == end_index):
         return
-    elif(type(l[i]) is str):
-        if(l[i] == 'int' or l[i] == 'float' or l[i] == 'return'):
-            output_prg+=[l[i],' ']
+    elif(type(parse_tree[start_index]) is str):
+        if(parse_tree[start_index]=='int' or parse_tree[start_index]=='float' or parse_tree[start_index]=='void' or parse_tree[start_index]=='return'):
+            # print("here",parse_tree[start_index])
+            output_prg += [parse_tree[start_index], ' ']
         else:
-            output_prg+=[l[i]]
-        solve(i+1,n,l,output_prg)
-    elif(type(l[i]) is int):
-        output_prg+=[str(l[i])]
-        solve(i+1,n,l,output_prg)
+            output_prg += [parse_tree[start_index]]
+        solve(start_index+1, end_index, parse_tree, output_prg)
+    elif(type(parse_tree[start_index]) is int):
+        output_prg += [str(parse_tree[start_index])]
+        solve(start_index+1, end_index, parse_tree, output_prg)
 
-    elif(type(l[i]) is tuple or type(l[i]) is list):
-        for j in range(len(l[i])):
-            if(type(l[i][j]) is tuple or type(l[i][j]) is list):
-                solve(0,len(l[i][j]),l[i][j],output_prg)
+    elif(type(parse_tree[start_index]) is tuple or type(parse_tree[start_index]) is list):
+        for trav in range(len(parse_tree[start_index])):
+            if(type(parse_tree[start_index][trav]) is tuple or type(parse_tree[start_index][trav]) is list):
+                solve(0, len(parse_tree[start_index][trav]), parse_tree[start_index][trav], output_prg)
             else:
-                if(l[i][j]=='int' or l[i][j]=='float' or l[i][j]=='return'):
-                    output_prg+=[str(l[i][j]),' ']
+                if(parse_tree[start_index][trav]=='int' or parse_tree[start_index][trav]=='float' or parse_tree[start_index][trav]=='void' or parse_tree[start_index][trav]=='return'):
+                    output_prg += [parse_tree[start_index][trav], ' ']
                 else:
-                    output_prg+=[str(l[i][j])]
-        solve(i+1,n,l,output_prg)
+                    output_prg += [str(parse_tree[start_index][trav])]
+        solve(start_index+1, end_index, parse_tree, output_prg)
 
 #-----------------------------------------------------------------------------------------------------------------------
