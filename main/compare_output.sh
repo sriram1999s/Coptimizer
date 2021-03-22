deecf7e0e981fe573e03df4d06f7798ad0e7a48a
sriram <sriramsub7@gmail.com>
2021-03-22 13:54:56 +0530

    compare_output complete

--- main/compare_output.sh
+++ main/compare_output.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-#./home/c0dey0da/Documents/Capstone/main/exec.sh $1
+sh ./exec.sh $1 > /dev/null
 gcc $1 -o inp
 gcc output.c -o out
 
+echo "\n\nprovide input for both ..\n"
 ./inp > inp_res
 ./out > out_res
 
-# diff inp_res out_res
-# echo $0
+echo "\ninp_res:"
+cat inp_res
 
+echo "\nout_res:"
+cat out_res
 
+echo "\ndiff : \n"
 
+echo `diff inp_res out_res`
+
+rm inp out inp_res out_res output.c
