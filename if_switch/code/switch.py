17c34db47afb9ef0e01be56111c62f3b0d765c6d
khusheimeda <43701562+khusheimeda@users.noreply.github.com>
2021-03-28 23:48:40 +0530

    updated and fixed handling extra brackets and breaks

--- if_switch/code/switch.py
+++ if_switch/code/switch.py
@@ -5,7 +5,6 @@ begin_net_open = 0
 dict_num_chain_pos = stack_match2.dict_num_list_of_chains.fromkeys(stack_match2.dict_num_list_of_chains.keys(), [0, 0])
 z_new = []
 dict_num_list_common_vars = dict()
-switched_window = -1
 
 
 def make_switch(z):
@@ -13,21 +12,11 @@ def make_switch(z):
     global dict_num_chain_pos
     global begin_net_open
     global z_new
-    global switched_window
     global dict_num_list_common_vars
 
     i = 0
     while i < len(z):
         print('in while', z[i], z_new)
-        # if switched_window!=-1 and switched_window==net_open:
-        #     i = skip_extra_brackets(i, z)
-        #     switched_window = -1
-        #     try:
-        #         if stack_match2.dict_num_list_of_chains[net_open][dict_num_chain_pos[net_open][0]][-1].type1 == 'elif':
-        #             print('in elif append')
-        #             z_new.append('}')
-        #     except:
-        #         print('chain pos not yet encountered')
 
         if z[i] == '{':
             net_open += 1
@@ -41,74 +30,63 @@ def make_switch(z):
 
         elif z[i] == 'if':
             begin_net_open = net_open
-            dict_num_chain_pos[net_open][1] += 1  # incremented object by one
+
+            # print('obj type', dict_num_chain_pos[net_open][1], stack_match2.dict_num_list_of_chains[net_open][dict_num_chain_pos[net_open][0]])
             chosen_var = check_change_to_switch(net_open)
+
+            # chain to be switched
+            # chosen var is the switch var
             if chosen_var is not None:
-                switched_window = net_open
                 main_list = stack_match2.dict_num_list_of_chains[net_open]
                 chain_pos = dict_num_chain_pos[net_open][0]
                 if_obj = main_list[chain_pos][0]
+
+                # list of tuples with chosen var
                 l = list(filter(lambda x: chosen_var in x, if_obj.condition_vars))
+
+                # by default choose the first tuple and get cmp_with value at index pos 1 of tuple
                 z_new.append('switch(' + chosen_var + ') { case ' + l[0][1] + ':')
+
+                # get how to deal with condition and where to read z from after handling that
                 pre_body, new_pos = get_new_prebody(i, z, chosen_var, l[0][1])
+
                 z_new.append(pre_body)
-                i = new_pos
+                i = util(new_pos, z, begin_net_open)
+
+                dict_num_chain_pos[net_open][1] += 1  # incremented object by one
+
+
+            # chain not switched
             else:
                 z_new.append(z[i])
                 i += 1
-            # dict_num_chain_pos[net_open][0] += 1
 
         elif z[i] == 'else':
             chosen_var = check_change_to_switch(begin_net_open)
 
+            # to be switched
             if chosen_var is not None:
                 main_list = stack_match2.dict_num_list_of_chains[begin_net_open]
                 chain_pos = dict_num_chain_pos[begin_net_open][0]
                 obj_no = dict_num_chain_pos[begin_net_open][1]
+                # print('obj no', obj_no)
                 chosen_chain = main_list[chain_pos]
                 obj = chosen_chain[obj_no]
                 if obj.type1 == 'elif':
-                    z_new.append('break;')
+                    dict_num_chain_pos[begin_net_open][1] += 1
+
                     l = list(filter(lambda x: chosen_var in x, obj.condition_vars))
                     z_new.append('case ' + l[0][1] + ':')
                     pre_body, new_pos = get_new_prebody(i + 4, z, chosen_var, l[0][1])
                     z_new.append(pre_body)
-                    i = new_pos
-                    begin_net_open1 = net_open
-                    while i < len(z):
-                        if z[i] == '{':
-                            net_open += 1
-                        elif z[i] == '}':
-                            net_open -= 1
-                        z_new.append(z[i])
-                        if net_open == begin_net_open1:
-                            break
-                        i += 1
-
-                    # can put the following 5 lines except i = skip_extra_brackets(i, z, net_open) line to the if condition in skip_extra_brackets where switched_window is being reset but it there is commonality in structure of elif and else case this way
-                    switched_window_before_reset = switched_window
-                    i_before_reset = i
-                    i = skip_extra_brackets(i, z, net_open)
-                    if i == i_before_reset - switched_window_before_reset:
-                        z_new.append('break;}')
+                    i = util(new_pos, z, net_open)
 
                 else:
-                    z_new.append('break;')
+                    dict_num_chain_pos[begin_net_open][1] += 1
+
                     z_new.append('default:')
                     i += 2
-                    begin_net_open1 = net_open
-                    while i < len(z):
-                        if z[i] == '{':
-                            net_open += 1
-                        elif z[i] == '}':
-                            net_open -= 1
-                        z_new.append(z[i])
-                        if net_open == begin_net_open1:
-                            break
-                        i += 1
-                    z_new.append('break;}')
-                    i = skip_extra_brackets(i, z, net_open)
-                dict_num_chain_pos[begin_net_open][1] += 1
+                    i = util(i, z, net_open)
 
             else:
                 z_new.append(z[i])
@@ -119,8 +97,9 @@ def make_switch(z):
             i += 1
 
 
+# needs to include a way to check for || and other cases where switch should not be done
 def check_change_to_switch(num):
-    global dict_num_list_common_vars
+    global dict_num_list_common_vars  # list of common variables for a chain
     # print('dict num list common vars')
     # for i in dict_num_list_common_vars:
     #     print(i, ':')
@@ -129,22 +108,27 @@ def check_change_to_switch(num):
     #             print(k, ', ', end='')
     #         print()
 
-    if num in dict_num_list_common_vars.keys():
+    # switch based on first common var in all if elif else objects of a chain
+    if num not in dict_num_list_common_vars.keys():
+        dict_num_list_common_vars[num] = []
+
+    elif num in dict_num_list_common_vars.keys():
         try:
             return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0]
-
         # not calculated for chain
         except:
             dict_num_list_common_vars[num][dict_num_chain_pos[num][0]] = []
 
-    # not calculated for any chain of num
-    dict_num_list_common_vars[num] = []
+    # not calculated for chain
     dict_num_list_common_vars[num].append([])
     main_list = stack_match2.dict_num_list_of_chains[num]
     chain_pos = dict_num_chain_pos[num][0]
     l = main_list[chain_pos].copy()  # l is a chain
+
+    # don't switch single if
     if len(l) == 1:
         return None
+    # else has no condition vars, so don't compare with that
     if l[-1].type1 == 'else':
         l.pop()
 
@@ -158,7 +142,6 @@ def check_change_to_switch(num):
         if count == len(l):
             dict_num_list_common_vars[num][dict_num_chain_pos[num][0]].append(i[0])
             return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0]
-            # return i[0]
         count = 1
     return None
 
@@ -166,61 +149,64 @@ def check_change_to_switch(num):
 def get_new_prebody(pos, z, var, cmp_with):
     global net_open
     global begin_net_open
-    indices = [i for i, x in enumerate(z) if x == '{']
+    indices = [i for i, x in enumerate(z) if x == '{']  # list of positions of { in z
     end_here = -1
+    # find first position of { after the curr pos
     for i in indices:
         if i > pos:
             end_here = i
             break
 
+    # look only from curr_pos + 1 till before a {
     z = z[pos + 1: end_here]
-    # print('z', z, pos, end_here)
-    indices = [i for i, x in enumerate(z) if x == var]
+    indices = [i for i, x in enumerate(z) if x == var]  # list of positions of var in shortened z
     ret = ''
     for i in indices:
         if i + 2 < len(z) and z[i + 1] == '==' and z[i + 2] == cmp_with:
-            if i - 2 >= 0 and z[i - 1] == '(' and z[i - 2] == '!' and z[i + 3] == ')':
-                ret += 'break;'
-                # j = i + 3
-                # while j < len(z):
-                #     if z[j] == '{':
-                #         net_open += 1
-                #     elif z[i] == '}':
-                #         net_open -= 1
-                #     if net_open == begin_net_open:  # think of other logic to skip body
-                #         return ret, j
-                #     j += 1
-                return ret, end_here
-            elif i - 1 >= 0 and z[i - 1] == '&&':
+            if i - 1 >= 0 and z[i - 1] == '&&':  # && before var
                 ret += 'if' + ''.join(z[:i - 1]) + ')'
-            elif i - 1 >= 0 and z[i - 1] == '||':
+
+            if i - 1 >= 0 and z[i - 1] == '(' and z[i + 3] == ')':  # condition is just var==cmp_with
                 return ret, end_here
 
-            if z[i - 1] == '(' and z[i + 3] == ')':  # condition is just var==cmp_with
-                return ret, end_here
-
-            if z[i + 3] == '&&':
+            if z[i + 3] == '&&':  # && after var
                 ret += 'if(' + ''.join(z[i + 4:])
-            elif z[i + 3] == '||':
-                return ret, end_here
     return ret, end_here
 
 
-def skip_extra_brackets(pos, z, num):
-    global switched_window
-    global dict_num_chain_pos
+def skip_extra_brackets(pos, z):
+    global begin_net_open
     i = pos
-    print('before', z[i])
     while i < len(z) and z[i] == '}':
         i += 1
 
-    # check logic to retain correct number of } after switched if
-    if i == pos + 1:
+    if i < len(z):
+        if z[i] == 'else':
+            z_new.append('break;')
+            return i
+        z_new.append('break;}')
         return i
-    if switched_window != -1:
-        temp = switched_window
-        switched_window = -1
-        dict_num_chain_pos[num][0] += 1
-        print('temp', temp, z[i - temp])
-        return i - temp
+
+    z_new.append('break;}')
+    for j in range(begin_net_open):
+        z_new.append('}')
+    return i
+
+
+def util(pos, z, begin_net_open1):
+    global net_open
+    i = pos
+    while i < len(z):
+        if z[i] == '{':
+            net_open += 1
+        elif z[i] == '}':
+            net_open -= 1
+        # append no matter which character
+        z_new.append(z[i])
+        if net_open == begin_net_open1:
+            i += 1
+            break
+        i += 1
+
+    i = skip_extra_brackets(i, z)
     return i
