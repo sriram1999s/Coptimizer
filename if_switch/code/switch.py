c5d47a53adfc3c4c910e4aa5e883a55e87de9839
khusheimeda <43701562+khusheimeda@users.noreply.github.com>
2021-04-02 16:30:08 +0530

    works for t-t4 and t7. t5 not to be switched

--- if_switch/code/switch.py
+++ if_switch/code/switch.py
@@ -78,6 +78,7 @@ def make_switch(z):
 
             # chain not switched
             else:
+                print('chain not switched')
                 z_new.append(z[i])
                 i += 1
 
@@ -161,22 +162,40 @@ def check_change_to_switch(num):
         l.pop()
 
     count = 1
-    for i in l[0].condition_vars:  # tuple
-        for j in l[1:]:  # obj
-            for k in j.condition_vars:  # tuple
-                if k[0] == i[0]:
-                    if k[1] == i[1]:    # const is the same
+    # for i in l[0].condition_vars:  # tuple
+    #     for j in l[1:]:  # obj
+    #         for k in j.condition_vars:  # tuple
+    #             if k[0] == i[0]:
+    #                 if k[1] == i[1]:    # const is the same
+    #
+    #                     # add check for switch on another variable that follows semantics
+    #                     return None
+    #
+    #                 count += 1
+    #                 break
+    #
+    #     if count == len(l):
+    #         dict_num_list_common_vars[num][dict_num_chain_pos[num][0]].append(i[0])
+    #         # print('returning', dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0])
+    #         return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0]
+    #     count = 1
 
-                        # add check for switch on another variable that follows semantics
-                        return None
-
-                    count += 1
-                    break
+    rhs = set()
+    if len(l[0].condition_vars)>0:
+        i = l[0].condition_vars[0]
+        rhs.add(i[1])
+        for j in l[1:]: # obj
+            k = j.condition_vars[0] # tuple
+            if k[0] == i[0]:
+                if k[1] in rhs:
+                    return None
+                rhs.add(k[1])
+                count += 1
         if count == len(l):
             dict_num_list_common_vars[num][dict_num_chain_pos[num][0]].append(i[0])
-            # print('returning', dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0])
+            print('returning', dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0])
             return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0]
-        count = 1
+
     return None
 
 
@@ -195,26 +214,87 @@ def get_new_prebody(pos, z, var, cmp_with):
     ret = ''
     for i in indices:
         if i + 2 < len(z) and z[i + 1] == '==' and z[i + 2] == cmp_with:
-            if i - 1 >= 0 and z[i - 1] == '&&':  # && before var
-                ret += 'if' + ''.join(z[:i - 1]) + ')'
+            # possible other condition before var
+            if i - 1 >= 0 and z[i - 1] == '(':
+                i_copy_l = i-2
+                while i_copy_l>=0 and z[i_copy_l] == '(':
+                    i_copy_l -= 1
+                # first condition
+                if i_copy_l<0:
+                    i_copy_r = i + 3
+                    while i_copy_r < len(z) and z[i_copy_r] == ')':
+                        i_copy_r += 1
+                    # only condition
+                    if i_copy_r == len(z):
+                        return ret, end_here
+                    # || after var==cmp_with in the beginning
+                    elif z[i_copy_r] == '||':
+                        return ret, end_here
+                    # && after var==cmp_with in the beginning
+                    elif z[i_copy_r] == '&&':
+                        ret = 'if(' + ''.join(z[i_copy_r + 1:])
+                        return ret, end_here
+                    # other operator like > or < or == after var==cmp_with in the beginning
+                    else:
+                        ret = 'if(' + ''.join(z[i_copy_r+1:])
+                        return ret
+                # not first condition
+                elif i_copy_l>=0:
+                    ret = 'if'
+                    return ret, pos+1
+            # not the first condition
+            elif i - 1 >= 0 and z[i - 1] != '(':
+                ret = 'if'
+                return ret, pos + 1
 
-            # not actually guaranteed
-            if z[i - 2] == 'if' and z[i - 1] == '(' and z[i + 3] == ')':  # condition is just var==cmp_with
-                return ret, end_here
+            # case of more ( and ) around the condition has to be considered
+            # if i - 2 >= 0 and i + 3 < len(z) and z[i - 2] == 'if' and z[i - 1] == '(' and z[i + 3] == ')':  # condition is just var==cmp_with
+                # return ret, end_here
 
-            if z[i + 3] == '&&':  # && after var
-                ret += 'if(' + ''.join(z[i + 4:])
+            # if i + 3 < len(z) and z[i + 3] == '&&':  # && after var
+            #     ret += 'if(' + ''.join(z[i + 4:])
 
         elif i - 2 >= 0 and z[i - 1] == '==' and z[i - 2] == cmp_with:
-            if i - 3 >= 0 and z[i - 3] == '&&':  # && before cmp_with
-                ret += 'if' + ''.join(z[:i - 3]) + ')'
+            # # case of more ( and ) around the condition has to be considered
+            # if i - 4 >= 0 and i + 1 < len(z) and z[i - 4] == 'if' and z[i - 3] == '(' and z[i + 1] == ')':  # condition is just cmp_with == var
+            #     return ret, end_here
+            #
+            # if i + 1 < len(z) and z[i + 1] == '&&':  # && after var
+            #     ret += 'if(' + ''.join(z[i + 2:])
 
-            # not actually guaranteed
-            if z[i - 4] == 'if' and z[i - 3] == '(' and z[i + 1] == ')':  # condition is just cmp_with == var
-                return ret, end_here
+            # possible other condition before var
+            if i - 3 >= 0 and z[i - 3] == '(':
+                i_copy_l = i - 4
+                while i_copy_l >= 0 and z[i_copy_l] == '(':
+                    i_copy_l -= 1
+                # first condition
+                if i_copy_l<0:
+                    i_copy_r = i + 1
+                    while i_copy_r < len(z) and z[i_copy_r] == ')':
+                        i_copy_r += 1
+                    # only condition
+                    if i_copy_r == len(z):
+                        return ret, end_here
+                    # || after var==cmp_with in the beginning
+                    elif z[i_copy_r] == '||':
+                        return ret, end_here
+                    # && after var==cmp_with in the beginning
+                    elif z[i_copy_r] == '&&':
+                        ret = 'if(' + ''.join(z[i_copy_r + 1:])
+                        return ret, end_here
+                    # other operator like > or < or == after var==cmp_with in the beginning
+                    else:
+                        ret = 'if(' + ''.join(z[i_copy_r + 1:])
+                        return ret
+                # not first condition
+                elif i_copy_l >= 0:
+                    ret = 'if'
+                    return ret, pos + 1
+            # not the first condition
+            elif i - 3 >= 0 and z[i - 3] != '(':
+                ret = 'if'
+                return ret, pos + 1
 
-            if z[i + 1] == '&&':  # && after var
-                ret += 'if(' + ''.join(z[i + 2:])
     return ret, end_here
 
 
