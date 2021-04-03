5db423f84f0a08d6dea022084928c54098ab7e7b
TheCodeYoda <smurf61988@gmail.com>
2021-04-03 19:26:51 +0530

    added tester

---
+++ main/output.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<time.h>
+int main()
+{
+	int a = 0;
+	int n;
+	scanf("%d", &n);
+	double startTime = (float)clock()/CLOCKS_PER_SEC;
+	int temp_8a69d4bf3c2b6818b9acf919a70d6c62 =
+	    (((n - (0)) / 1) + (((n - (0)) % 1) != 0));
+	for (int i = 0;
+	     i <
+	     (temp_8a69d4bf3c2b6818b9acf919a70d6c62 -
+	      (temp_8a69d4bf3c2b6818b9acf919a70d6c62 % 2)) / 2; i++) { {
+			a++;
+	} {
+		a++;
+	}} if ((temp_8a69d4bf3c2b6818b9acf919a70d6c62 % 2)) { {
+			a++;
+	}
+	}
+	double endTime = (float)clock()/CLOCKS_PER_SEC;
+	double timeElapsed = endTime - startTime;
+	printf("%d\n", a);
+	printf("%lf\n",timeElapsed);
+}
+
+
+
+
+
+
+
+
