5085c1ed320d8dea01b311d0dd92ddced8558588
khusheimeda <43701562+khusheimeda@users.noreply.github.com>
2021-04-04 20:06:26 +0530

    works for t9 range. need to check t1 and other cases

--- if_switch/code/switch.py
+++ if_switch/code/switch.py
@@ -36,7 +36,6 @@ def make_switch(z):
             z_new.append(z[i])
 
             if order and order[-1][1] == net_open:
-                print('order', order)
                 i = skip_extra_brackets(i + 1, z)
                 order.pop()
             else:
@@ -51,7 +50,9 @@ def make_switch(z):
 
             beg_net_open_if = net_open
 
-            chosen_var = check_change_to_switch(net_open)
+            # chosen_var = check_change_to_switch(net_open)
+
+            chosen_var, range_lower_bound = check_change_to_switch(net_open)
 
             # chain to be switched
             # chosen var is the switch var
@@ -60,14 +61,18 @@ def make_switch(z):
                 chain_pos = dict_num_chain_pos[net_open][0]
                 if_obj = main_list[chain_pos][0]
 
-                # list of tuples with chosen var
-                l = list(filter(lambda x: chosen_var in x, if_obj.condition_vars))
+                # # list of tuples with chosen var
+                # l = list(filter(lambda x: chosen_var in x, if_obj.condition_vars))
+                #
+                # # by default choose the first tuple and get cmp_with value at index pos 1 of tuple
+                # z_new.append('switch(' + chosen_var + ') { case ' + l[0][1] + ':')
+                #
+                # # get how to deal with condition and where to read z from after handling that
+                # pre_body, new_pos = get_new_prebody(i, z, chosen_var, l[0][1])
 
-                # by default choose the first tuple and get cmp_with value at index pos 1 of tuple
-                z_new.append('switch(' + chosen_var + ') { case ' + l[0][1] + ':')
-
-                # get how to deal with condition and where to read z from after handling that
-                pre_body, new_pos = get_new_prebody(i, z, chosen_var, l[0][1])
+                case_no, range_case = get_case_no(if_obj, chosen_var, range_lower_bound)
+                z_new.append('switch(' + chosen_var + ') { case ' + case_no + ':')
+                pre_body, new_pos = get_new_prebody(i, z, chosen_var, case_no, range_case)
 
                 z_new.append(pre_body)
                 i = new_pos
@@ -84,7 +89,9 @@ def make_switch(z):
                 i += 1
 
         elif z[i] == 'else':
-            chosen_var = check_change_to_switch(seen_at_num[-1])
+            # chosen_var = check_change_to_switch(seen_at_num[-1])
+
+            chosen_var, range_lower_bound = check_change_to_switch(seen_at_num[-1])
 
             # to be switched
             if chosen_var is not None:
@@ -100,9 +107,14 @@ def make_switch(z):
 
                     beg_net_open_elif = net_open
 
-                    l = list(filter(lambda x: chosen_var in x, obj.condition_vars))
-                    z_new.append('case ' + l[0][1] + ':')
-                    pre_body, new_pos = get_new_prebody(i + 4, z, chosen_var, l[0][1])
+                    # l = list(filter(lambda x: chosen_var in x, obj.condition_vars))
+                    # z_new.append('case ' + l[0][1] + ':')
+                    # pre_body, new_pos = get_new_prebody(i + 4, z, chosen_var, l[0][1])
+
+                    case_no, range_case = get_case_no(obj, chosen_var, range_lower_bound)
+                    z_new.append('case ' + case_no + ':')
+                    pre_body, new_pos = get_new_prebody(i, z, chosen_var, case_no, range_case)
+
                     z_new.append(pre_body)
                     i = new_pos
 
@@ -135,10 +147,12 @@ def check_change_to_switch(num):
     # switch based on first common var in all if elif else objects of a chain
     if num not in dict_num_list_common_vars.keys():
         dict_num_list_common_vars[num] = []
-
     elif num in dict_num_list_common_vars.keys():  # calculated for some chain at num earlier
         try:
-            return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0]  # calculated for same chain already
+            # return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0]
+
+            return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0], None # calculated for same chain already
+
         # not calculated for chain
         except:
             print('error')
@@ -148,43 +162,83 @@ def check_change_to_switch(num):
         dict_num_list_common_vars[num].append([])
         main_list = stack_match2.dict_num_list_of_chains[num]
         chain_pos = dict_num_chain_pos[num][0]
-        # print('len', num, len(stack_match2.dict_num_list_of_chains[num]))
-        l = main_list[chain_pos].copy()  # l is a chain
+        l_chain = main_list[chain_pos].copy()  # l_chain is a chain
 
     except:
-        return None
+        # return None
+
+        return None, None
 
     # don't switch single if
-    if len(l) == 1:
-        return None
+    if len(l_chain) == 1:
+        # return None
+
+        return None, None
+
     # else has no condition vars, so don't compare with that
-    if l[-1].type1 == 'else':
-        l.pop()
+    if l_chain[-1].type1 == 'else':
+        l_chain.pop()
 
     count = 1
-
     rhs = set()
-    if len(l[0].condition_vars) > 0:
-        i = l[0].condition_vars[0]
+    if len(l_chain[0].condition_vars) > 0:
+        i = l_chain[0].condition_vars[0]
         rhs.add(i[1])
-        for j in l[1:]:  # obj
+        for j in l_chain[1:]:  # obj
             if len(j.condition_vars) == 0:
-                return None
+                # return None
+
+                return None, None
+
             k = j.condition_vars[0]  # tuple
             if k[0] == i[0]:
                 if k[1] in rhs:
-                    return None
+                    # return None
+
+                    return None, None
+
                 rhs.add(k[1])
                 count += 1
-        if count == len(l):
+        if count == len(l_chain):
             dict_num_list_common_vars[num][dict_num_chain_pos[num][0]].append(i[0])
             # print('returning', dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0])
-            return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0]
+            # return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0]
 
-    return None
+            return dict_num_list_common_vars[num][dict_num_chain_pos[num][0]][0], None
+
+    elif l_chain[0].range_var is not None:
+        seq = [(l_chain[0].l, l_chain[0].u)]
+        width = calculate_width(l_chain[0])
+        print('in elif', width)
+        for j in l_chain[1:]:
+            if j.range_var is not None and j.range_var == l_chain[0].range_var:
+                if l_chain[0].op1 == j.op1 and l_chain[0].op2 == j.op2 and width==calculate_width(j):
+                    seq.append((j.l, j.u))
+                else:
+                    return None, None
+            else:
+                return None, None
+        sorted(seq, key=lambda x: x[0])
+        for i2 in range(1, len(seq)):
+            if l_chain[0].op1=='<=' and l_chain[0].op2=='<=' and seq[i2][0]-1 != seq[i2-1][1]:
+                return None, None
+            if l_chain[0].op1=='<=' and l_chain[0].op2=='<' and seq[i2][0] != seq[i2-1][1]:
+                return None, None
+            if l_chain[0].op1 == '<' and l_chain[0].op2 == '<=' and seq[i2][0] != seq[i2 - 1][1]:
+                return None, None
+            if l_chain[0].op1=='<' and l_chain[0].op2=='<' and seq[i2][0] != seq[i2-1][1]-1:
+                return None, None
+
+        # dict_num_list_common_vars[num][dict_num_chain_pos[num][0]].append([l_chain[0], l_chain[0].l])
+        return switch_range_condition(l_chain[0]), l_chain[0].l
+
+    # return None
+
+    return None, None
 
 
-def get_new_prebody(pos, z, var, cmp_with):
+# def get_new_prebody(pos, z, var, cmp_with):
+def get_new_prebody(pos, z, var, cmp_with, range_case):
     indices = [i for i, x in enumerate(z) if x == '{']  # list of positions of { in z
     end_here = -1
     # find first position of { after the curr pos
@@ -192,6 +246,13 @@ def get_new_prebody(pos, z, var, cmp_with):
         if i > pos:
             end_here = i
             break
+
+    if range_case:
+        if z[pos] == 'if':
+            return '', end_here
+        for i in indices:
+            if i > end_here:
+                return '', i
 
     # look only from curr_pos + 1 till before a {
     z = z[pos + 1: end_here]
@@ -298,3 +359,44 @@ def skip_extra_brackets(pos, z):
                 net_open -= 1
                 pos += 1
             return pos
+
+
+def calculate_width(obj):
+    if (obj.op1=='<=' and obj.op2=='<=') or (obj.op1=='<' and obj.op2=='<'):
+        return obj.u-obj.l+1
+    if (obj.op1=='<=' and obj.op2=='<') or (obj.op1=='<' and obj.op2=='<='):
+        return obj.u-obj.l
+
+
+def switch_range_condition(obj):
+    condition = ''
+    if obj.op1 == '<=' and obj.op2 == '<=':
+        w = obj.u-obj.l+1
+        condition += '(' + obj.range_var + '-' + str(obj.l) + ')/' + str(w)
+        return condition
+    if obj.op1 == '<=' and obj.op2 == '<':
+        w = obj.u-obj.l
+        condition += '(' + obj.range_var + '-' + str(obj.l) + ')/' + str(w)
+        return condition
+    if obj.op1 == '<' and obj.op2 == '<=':
+        w = obj.u - obj.l
+        condition += '(' + obj.range_var + '-(' + str(obj.l+1) + '))/' + str(w)
+        return condition
+    if obj.op1 == '<' and obj.op2 == '<':
+        w = obj.u - obj.l + 1
+        condition += '(' + obj.range_var + '-(' + str(obj.l+1) + '))/' + str(w)
+        return condition
+
+
+def get_case_no(obj, chosen_var, range_lower_bound):
+    if obj.condition_vars != []:
+        return list(filter(lambda x: chosen_var in x, obj.condition_vars))[0][1], False
+    if obj.range_var is not None:
+        if obj.op1 == '<=' and obj.op2 == '<=':
+            return str((obj.l-range_lower_bound)/(obj.u-obj.l+1)).split('.')[0], True
+        if obj.op1 == '<=' and obj.op2 == '<':
+            return str((obj.l-range_lower_bound)/(obj.u-obj.l)).split('.')[0], True
+        if obj.op1 == '<' and obj.op2 == '<=':
+            return str((obj.l-(range_lower_bound+1))/(obj.u-obj.l)).split('.')[0], True
+        if obj.op1 == '<' and obj.op2 == '<':
+            return str((obj.l-(range_lower_bound+1))/(obj.u-obj.l+1)).split('.')[0], True
