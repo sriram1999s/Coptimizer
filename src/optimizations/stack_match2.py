fefee80a45e1138f0bbe68117c0201ebdef8fb5c
Khushei Meghana Meda <43701562+khusheimeda@users.noreply.github.com>
2021-07-04 23:18:19 +0530

    Added documentation comments

--- src/optimizations/stack_match2.py
+++ src/optimizations/stack_match2.py
@@ -1,3 +1,12 @@
+''' details of every if, else if, and else condition
+type1 - indicates whether the object represents an if condition, else if condition, or else case
+condition_vars - the list of variables used in the conditional statements
+range_var - if the variable used in the condition is being checked for being part of uniform, unbroken ranges
+l - lower bound of range cases
+u - upper bound of range cases
+op1 - the operator between range_var and the lower bound (<, <=, >, >=)
+op2 - the operator between range_var and the upper bound (<, <=, >, >=)
+'''
 class if_elif_else:
     def __init__(self, type1, condition_vars, range_var=None, l=None, u=None, op1=None, op2=None):
         self.type1 = type1
@@ -9,7 +18,9 @@ class if_elif_else:
         self.op2 = op2
         self.range_var = range_var
 
-
+        
+''' key: the number of open braces when the chain starts, it denotes the nesting level of the chain
+value: list of chains of if_elif_else objects '''
 dict_num_list_of_chains = dict()
 
 
@@ -32,12 +43,18 @@ def is_char(s):
     return False
 
 
+''' identify matching if, else if, and else conditions and store each such chain in a common list '''
 def identify_chains(OPTIMIZE, z):
     if (not OPTIMIZE):
         return
     global dict_num_list_of_chains
     net_open = 0
 
+    ''' Go through the parse tree element by element while maintaining the number of open braces in the variable net_open. 
+    This variable helps to identify if, else if, and else which are associated with each other. 
+    For an 'if' condition, create an object with all the its condition details. If it is the first 'if' at that level of nesting, create a new key in the global dictionary with value as a list containing the object. If is not the first 'if' at that level of nesting, a new key need not be created and the new list with this object can be appended to the existing key. 
+    For an 'else if' or 'else' condition, create an object with all the its condition details and append this object to the list containing the corresponding 'if' condition object. 
+    '''
     i = 0
     while i < len(z):
 
@@ -83,12 +100,14 @@ def create_obj(type1, pos, z):
         return obj
 
 
+''' helper function '''
 def skip_while(pos, ch, z):
     while pos < len(z) and z[pos] == ch:
         pos += 1
     return pos
 
 
+''' helper function that helps to store the operators in a uniform manner in the object '''
 def lateral_op(op):
     if op == '<':
         return '>'
@@ -100,13 +119,14 @@ def lateral_op(op):
         return '<='
 
 
+''' check whether the condition is of the type that can be switched, or if it compares a variable with a uniform, unbroken range. Create and return an object accordingly ''' 
 def util(type1, z, i):
     l1 = []
     range_ops = ['<=', '>=', '<', '>']
     i = skip_while(i, '(', z)
     if i + 2 < len(z) and z[i + 1] == '==':
         if (is_int(z[i]) or is_char(z[i])) and not is_int(z[i + 2]):
-            # if after is || don't add var to list because should not switch
+             # if after is || don't add var to list because should not switch 
             if z[i + 3] != '||':
                 l1.append((z[i + 2], z[i]))
         elif (is_int(z[i + 2]) or is_char(z[i + 2])) and not is_int(z[i]):
