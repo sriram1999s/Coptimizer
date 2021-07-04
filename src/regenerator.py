5c35b7a68d677f4aec37872668c6d5e05cb4222f
sriram1999s <sriramsub7@gmail.com>
2021-07-04 11:49:33 +0530

    cleaned

--- src/regenerator.py
+++ src/regenerator.py
@@ -6,13 +6,11 @@ import uuid
 import sys
 import re
 import itertools
-# temp_list1 = []
-# temp_list2 = []
+
 
 # ----------------------------------------------fn inliner -----------------------------------------------------------
 '''function inlining recursive handler'''
 def fn_inline_solve(i,n,z,cyc_chk,non_in_fn):
-    ##print("cyc_chk1 :",cyc_chk)
     if(i == n):
         return
     elif(type(z[i]) is tuple):
@@ -23,22 +21,18 @@ def fn_inline_solve(i,n,z,cyc_chk,non_in_fn):
                 name_fn = z[i][0]
                 #---------------par_to_arg_match-----------------------------
                 fn_body = copy.deepcopy(fn_defn_obj_list[defn_t_ix].body)
-                ##print(z[i][0],"fn_body :\n",fn_body,"\n\n")
                 fn_body[0].pop(0)
                 fn_body[0].pop(-1)
 
-                #param_hash = secrets.token_hex(nbytes=12)
                 param_hash = uuid.uuid4().hex
                 parameter_list1 = copy.deepcopy(fn_defn_obj_list[defn_t_ix].param_list)
                 parameter_list2 = get_param_vars(parameter_list1)
                 add_param_hash(param_hash,parameter_list1)
                 replace_param(0 , len(fn_body) , fn_body , parameter_list2 , param_hash)
-                ##print(z[i][0],"parameter_list1 : ",parameter_list1)
                 par_arg_match = " "
                 for ix in range(len(parameter_list1)):
                     param = parameter_list1[ix]
                     arg = fn_call_obj_list[call_t_ix].arg_list[ix]
-                    ##print(z[i][0],"param :",param,"type :",type(param),"arg :",arg,"type :",type(arg))
                     par_arg_match += (param + ' = ' + arg + ' ; ')
 
                 par_arg_match += " "
@@ -58,11 +52,8 @@ def fn_inline_solve(i,n,z,cyc_chk,non_in_fn):
                 ret_val_str = ""
                 some_temp = ""
                 ret_value = fn_defn_obj_list[defn_t_ix].return_id_or_val
-                ##print("\nret_value :",z[i][0],ret_value,"\n")
                 fn_call_expr_flag = 0
                 if(ret_value):
-                    #if(ret_value in parameter_list2):
-                    #    ret_value += "_" + param_hash
                     if(len(z[i]) == 5):
                         ret_dec = z[i][3] + " " + z[i][4] + " ;\n" + z[i][3] + " temp_" + param_hash + ";\n"
                         fn_body.insert(0,ret_dec)
@@ -97,7 +88,6 @@ def fn_inline_solve(i,n,z,cyc_chk,non_in_fn):
                 elif(fn_call_expr_flag == 62):
                     fn_body.append('\nreturn ' + "temp_" + param_hash + " ;\n")
 
-                ##print(z[i][0],"fn_bodyyy\n",fn_body)
                 z.insert(i,fn_body)
                 z.pop(i + 1)
                 if(i + 1 < len(z) and fn_call_expr_flag == 4):
@@ -116,12 +106,10 @@ def fn_inline_solve(i,n,z,cyc_chk,non_in_fn):
                     ret_dec = " " + z[i][3] + " " + z[i][4] + " = "
                     fn_call1.append(ret_dec)
                     fn_call1.append(z[i][1])
-                    #fn_call1.append(';')
                 elif(len(z[i]) == 4):
                     ret_dec = " " + z[i][3] + " = "
                     fn_call1.append(ret_dec)
                     fn_call1.append(z[i][1])
-                    #fn_call1.append(';')
                 elif(len(z[i]) == 6):
                     if(z[i][-1] == "return"):
                         fn_call1 = z[i][1]
@@ -135,11 +123,8 @@ def fn_inline_solve(i,n,z,cyc_chk,non_in_fn):
                 z.pop(i+1)
                 fn_inline_solve(0,len(z[i]),z[i],cyc_chk,non_in_fn)
         else:
-            ##print("CAME HERE")
             defn_t_ix = get_defn_t_ix(z[i][0])
             fn_obj2 = fn_defn_obj_list[defn_t_ix];
-            ##print(fn_obj2.name)
-            ##print("defn :",z[i][0])
             fn_defn = z[i][1]
             z.insert(i,fn_defn)
             z.pop(i + 1)
@@ -197,36 +182,7 @@ def tail_rec_eli_solve(in_flag1, i,n,z):
 
 # ----------------------------------------------code generator ------------------------------------------------------
 
-# def solve(start_index, end_index, parse_tree, output_prg):
-#     space_list = ['int','float','void','double','bool','char','return','else','if']
-#     # space_list = ['return']
-#     if(start_index == end_index):
-#         return
-#     elif(parse_tree[start_index]==None):
-#          solve(start_index+1, end_index, parse_tree, output_prg)
-#     elif(type(parse_tree[start_index]) is str):
-#         if(start_index+1<end_index and parse_tree[start_index] in space_list and parse_tree[start_index+1]!=' '):
-#             output_prg += [parse_tree[start_index], ' ']
-#         else:
-#             output_prg += [parse_tree[start_index]]
-#         solve(start_index+1, end_index, parse_tree, output_prg)
-#     elif(type(parse_tree[start_index]) is int):
-#         output_prg += [str(parse_tree[start_index])]
-#         solve(start_index+1, end_index, parse_tree, output_prg)
-#
-#     elif(type(parse_tree[start_index]) is tuple or type(parse_tree[start_index]) is list):
-#         for trav in range(len(parse_tree[start_index])):
-#             if(parse_tree[start_index][trav]==None):
-#                 continue
-#             elif(type(parse_tree[start_index][trav]) is tuple or type(parse_tree[start_index][trav]) is list):
-#                 solve(0, len(parse_tree[start_index][trav]), parse_tree[start_index][trav], output_prg)
-#             else:
-#                 print("in solve ", parse_tree[start_index], trav + 1, end_index)
-#                 if(trav+1<end_index and parse_tree[start_index][trav] in space_list and parse_tree[start_index][trav+1]!=' '):
-#                     output_prg += [parse_tree[start_index][trav], ' ']
-#                 else:
-#                     output_prg += [str(parse_tree[start_index][trav])]
-#         solve(start_index+1, end_index, parse_tree, output_prg)
+
 
 '''recursive regenerator'''
 def solve(start_index, end_index, parse_tree):
@@ -252,7 +208,7 @@ def solve(start_index, end_index, parse_tree):
     '''recurse into the sublist if type is list'''
     elif(type(parse_tree[start_index])==list):
         return solve(0, len(parse_tree[start_index]), parse_tree[start_index]) + solve(start_index+1, end_index, parse_tree)
-    
+
 
 
 '''multi threaded solve (Buggy)'''
@@ -270,5 +226,4 @@ def solve_multithread(start_index,end_index,parse_tree):
 
     return output_prg
 
-    # return result
 # -----------------------------------------------------------------------------------------------------------------------
