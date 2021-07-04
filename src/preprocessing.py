1418ed79b2474abd011a26b9884c0b0576b88c77
TheCodeYoda <smurf61988@gmail.com>
2021-07-04 11:54:34 +0530

    commented preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,18 +1,24 @@
 import re
 import os
 
+'''preprocessing handler'''
 def pre_process(text):
+    '''regex to detect <=/>= to </> for operator consistency'''
     pat1 = '(for\s*\(.*?)(<|>)=\s*(.*?)([\s;)])'
     text = re.sub(pat1 ,remove_rel_assign ,text)
-    # pat2 = 'void.*?{(.*?)}'
+
+    '''regex to detect single line commaents and convert to multiline comments'''
     pat3 = r'//(.*?)\n'
     text = re.sub(pat3, change_to_multiline, text)
+    
+    '''regex to detect newline and remove it'''
     pat4 = r'\n'
     text = re.sub(pat4, '', text)
-    # re.sub(pat2 ,return_void ,text)
+   
     print("Printing preprocessed text...\n")
     print(text,"\n\n")
 
+    '''flag generation for input detection in the C code'''
     dir_path = os.environ['COPTIMIZER_PATH']
     with open(f'{dir_path}/env/check_input', 'w') as file:
         check_inp = '(?:scanf\()|(?:gets\()|(?:getc\()'
@@ -22,10 +28,9 @@ def pre_process(text):
             file.write('0')
     return text
 
+'''replaces relational operators(called when pat1 matched)'''
 def remove_rel_assign(m):
-    # print(m.groups(0))
     if(m.group(2)=='<'):
-        # print("in pre_process")
         if(re.search('[a-zA-z_]+',m.group(3))):
             new_val = m.group(3)+'+1'
         else:
@@ -37,9 +42,6 @@ def remove_rel_assign(m):
             new_val = str(int(m.group(3))+1)
     return m.group(1) + m.group(2)  + new_val  + m.group(4)
 
+'''callback to replace single line comments to multiline comments(pat3)'''
 def change_to_multiline(m):
-    # print(m.group(1))
     return '/*' + m.group(1) + '*/'
-# def return_void(m):
-    # print("here")
-    # print(m.groups(0))
