ad03a5f5fe144d612569f239535cb76f0811d317
sriram1999s <>
2021-08-26 12:50:52 +0530

    changed inp

--- input_files/sentinels/sentinel1.c
+++ input_files/sentinels/sentinel1.c
@@ -5,7 +5,7 @@ int main()
   /*data-structure:array*/
   int a[] = {5, 8, 3, 2, 12, 17, 19, 30};
   int n = sizeof(a) / sizeof(int);
-  int elem = 19;
+  int elem = 190;
   int i = 0;
   /*linear-search-begin*/
   while (i < n) {
