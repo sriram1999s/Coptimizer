65f8ca7e102040e2d14415ee9aa2348178325c7b
TheCodeYoda <smurf61988@gmail.com>
2021-09-03 12:13:42 +0530

    made necessory changes to handle multiple if cases with breaks

--- src/optimizations/sentinel.py
+++ src/optimizations/sentinel.py
@@ -28,12 +28,19 @@ class Sentinel:
         import re
         if(sub_tree[0] == 'while'):
             bounds_condition = sub_tree[1]
-            found_condition, found_body = self.detect_and_remove_if(sub_tree[2])
+            found_condition_raw, found_body = self.detect_and_remove_if(sub_tree[2])
+
+            # chain multiple conditions using ||
+            found_condition_formatted = []
+            for condition in found_condition_raw:
+                found_condition_formatted.append(''.join([str(j) for j in flatten(condition)]))
+                
+            found_condition = ' || '.join(found_condition_formatted)
             # print(f"\n\n linear_to_sentinel : {found_condition}, {found_body}")
-            sub_tree[1] = ['(', '!', found_condition , ')']
-            found_body_new = re.sub('break\s*?;', '', ''.join(flatten(found_body)))
+            sub_tree[1] = ['!', '(',  found_condition, ')']
+            found_body_new = re.sub(r'break\s*?;', '', ''.join(flatten(found_body)))
             # print("fnjhkdv : ", found_body_new, " @ ", ''.join(flatten(found_body)))
-            insert_code, name, hash = self.add_sentinel(found_condition)
+            insert_code, name, hash = self.add_sentinel(found_condition_raw[0])
             print("\n\ninsert code : ", insert_code)
             bounds_condition.insert(-1, '-')
             bounds_condition.insert(-1, '1')
@@ -46,15 +53,15 @@ class Sentinel:
         import re
         def check_break(sub_tree):
             flattened_tree = ''.join(flatten(sub_tree))
-            if(re.search("break\s*?;", flattened_tree)):
+            if(re.search(r"break\s*?;", flattened_tree)):
                 return True
             return False
 
         def check_instructions(sub_tree):
             flattened_list = flatten(sub_tree)
             for elem in flattened_list:
-                #print(elem)
-                if(re.search("[-+*/=]",str(elem))):
+                # print(elem)
+                if(re.search("[-+*/=]", str(elem))):
                     return False
             return True
 
@@ -62,35 +69,34 @@ class Sentinel:
             if(i == n):
                 return
             if(type(sub_tree[i]) == list and sub_tree[i][0] == 'if' and check_break(sub_tree[i][2]) and check_instructions(sub_tree[i][2])):
+                # print("asdasd ",sub_tree[i][1])
                 condition.append(sub_tree[i][1])
                 body.append(sub_tree[i][2])
                 sub_tree[i] = []
-                return
 
             if(type(sub_tree[i]) == list):
-                find(0 , len(sub_tree[i]), sub_tree[i], condition, body)
+                find(0, len(sub_tree[i]), sub_tree[i], condition,body)
 
-            find(i+1 , n, sub_tree, condition, body)
-
+            find(i+1, n, sub_tree, condition, body)
 
         condition_list = []
         body_list = []
         find(0, len(sub_tree), sub_tree, condition_list, body_list)
-        return (condition_list[-1], body_list[-1])
+        return (condition_list, body_list[-1])
 
     ''' adds sentinel value to end of tagged ds '''
     def add_sentinel(self, condition):
         import re
         flattened_condition = ''.join(flatten(condition))
-        flattened_condition = re.sub("\(", " ( ", flattened_condition)
-        flattened_condition = re.sub("\)", " ) ", flattened_condition)
-        pat = "\s([^()]*?)\["
+        flattened_condition = re.sub(r"\(", r" ( ", flattened_condition)
+        flattened_condition = re.sub(r"\)", r" ) ", flattened_condition)
+        pat = r"\s([^()]*?)\["
         m = re.search(pat, flattened_condition)
         name = ''
         if(m):
             name = m.group(1)
 
-        pat = "==\s*(.*?)\s"
+        pat = r"==\s*(.*?)\s"
         m = re.search(pat, flattened_condition)
         sentinel = ''
         if(m):
@@ -103,12 +109,13 @@ class Sentinel:
         code += f" {name}[n{hash} - 1] = {sentinel};"
         return code, name, hash
 
+
 def increase_size_array(statement,type_ds):
     if(type_ds == "array"):
         if(type(statement[2][1]) == int):
             statement[2][1] += 1
         else:
-            recursive_insert(0,len(statement[4]),-1,statement[4])
+            recursive_insert(0, len(statement[4]), -1, statement[4])
 
 def check_nested(l):
     for sub in l:
@@ -121,14 +128,14 @@ def recursive_insert(ind,n,elem,statement):
     if(ind == n):
         return
     if(type(statement[ind]) == list and check_nested(statement[ind]) and len(statement[ind])==3):
-        temp = [1,',',1]
+        temp = [1, ',', 1]
         temp[0] = statement[ind][-1]
         temp[-1] = elem
         statement[ind][-1] = temp
         return
     if(type(statement[ind]) == list):
-        recursive_insert(0,len(statement[ind]),elem,statement[ind])
+        recursive_insert(0, len(statement[ind]), elem, statement[ind])
     else:
-        recursive_insert(ind+1,n,elem,statement)
+        recursive_insert(ind+1, n, elem, statement)
 
 sentinel = Sentinel()
