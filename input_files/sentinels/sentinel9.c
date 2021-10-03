e780584d05dca1920eea88b854a02249bcb7eba7
TheCodeYoda <smurf61988@gmail.com>
2021-10-03 20:25:37 +0530

    test file addition

---
+++ input_files/sentinels/sentinel9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* predicate-begin */
+int predicate(int a)
+{
+  return (a >> 1) & 1;
+}
+/* predicate-end */
+
+int main()
+{
+  /*data-structure:array*/
+  int a[] = {5, 8, 3, 2, 12, 17, 19, 32};
+  int n = sizeof(a) / sizeof(int);
+  int elem = 2;
+  int i = 0;
+  /*linear-search-begin*/
+  while (i < n) {
+    if (predicate(a[i])) {
+      printf("Predicate True\n");
+      break;
+    }
+    i++;
+  }
+  /*linear-search-end*/
+}
