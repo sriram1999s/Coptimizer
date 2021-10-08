0474f076ff5a4c65f9bdb595c11e778078e94253
sriram1999s <>
2021-10-08 20:57:25 +0530

    new input

--- input_files/cache/matrix1.c
+++ input_files/cache/matrix1.c
@@ -3,15 +3,8 @@
 #include <math.h>
 #include <time.h>
 
-double time_elapsed(struct timespec start, struct timespec end)
-{
-	double t;
-	t = (end.tv_sec - start.tv_sec);
-	t += (end.tv_nsec - start.tv_nsec) * 0.000000001;
-	return t;
-}
 
-void init(int n; double a[][n], int n)
+void init(double a[][n], int n)
 {
 	for(int i = 0; i < n; ++i)
 	{
@@ -22,8 +15,7 @@ void init(int n; double a[][n], int n)
 	}
 
 }
-// A[l, m], B[m, n], C[l, n]
-void multiply(int n; double a[][n], double b[][n], double c[][n], int l, int m, int n)
+void multiply(double a[][n], double b[][n], double c[][n], int l, int m, int n)
 {
 	for(int i = 0; i < l; ++i)
 	{
@@ -47,13 +39,7 @@ int main()
 	double c[n][n];
 	init(a, n);
 	init(b, n);
-	struct timespec start;
-	struct timespec end;
-	clock_gettime(CLOCK_REALTIME, &start);
 	multiply(a, b, c, n, n, n);
-	clock_gettime(CLOCK_REALTIME, &end);
-	printf("time %lf \n",
-			   time_elapsed(start, end));
 	printf("the end\n");
 
 }
