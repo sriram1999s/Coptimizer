13d223d77e62dadba42df86d00cd8aa2b8c3bf52
TheCodeYoda <smurf61988@gmail.com>
2021-10-18 11:28:23 +0530

    fixed for41.c for producing the proper output

--- input_files/unrolling/for41.c
+++ input_files/unrolling/for41.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
-#include<time.h>
-int main() {
-    int a=0;
-    int n;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++) {
-	     a = a + i;
-    }
-    printf("%d\n", a);
+#include <stdio.h>
+#include <time.h>
+int main()
+{
+  int a = 0;
+  int n;
+  scanf("%d", &n);
+  int c_i = 0;
+  for (int i = 0; i < n; i++) {
+    a = a + c_i;
+    ++c_i;
+  }
+  printf("%d\n", a);
 }
