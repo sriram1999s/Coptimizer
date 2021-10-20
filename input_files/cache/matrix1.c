21e4e1ed5b82bb6f7397688482b2204366074a95
sriram1999s <>
2021-10-20 20:51:06 +0530

    changed input

--- input_files/cache/matrix1.c
+++ input_files/cache/matrix1.c
@@ -19,7 +19,6 @@ void multiply(double a[][n], double b[][n], double c[][n], int l, int m, int n)
 {
   for (int i = 0; i < l; ++i) {
     for (int j = 0; j < n; ++j) {
-      c[i][j] = 0.0;
       for (int k = 0; k < m; ++k) {
         c[i][j] += a[i][k] * b[k][j];
       }
