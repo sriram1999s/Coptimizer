c36c9e27b531953d72ae47ffb6f531f9c0281918
sriram1999s <>
2021-10-21 20:17:29 +0530

    input formatting

--- input_files/cache/matrix1.c
+++ input_files/cache/matrix1.c
@@ -3,37 +3,37 @@
 #include <math.h>
 #include <time.h>
 
-/* void init(double a[][n], int n) */
-/* { */
-/* 	for(int i = 0; i < n; ++i) */
-/* 	{ */
-/* 		for(int j = 0; j < n; ++j) */
-/* 		{ */
-/* 			a[i][j] = rand(); */
-/* 		} */
-/* 	} */
+int n = 256;
+double c[256][256];
+ void init(double a[][n])
+ {
+ 	for(int i = 0; i < n; ++i)
+ 	{
+ 		for(int j = 0; j < n; ++j)
+ 		{
+ 			a[i][j] = rand();
+ 		}
+ 	}
 
-/* } */
-
-void multiply(double a[][n], double b[][n], double c[][n], int l, int m, int n)
+ }
+void multiply(double a[][n], double b[][n], double c[][n])
 {
-  for (int i = 0; i < l; ++i) {
+  for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      for (int k = 0; k < m; ++k) {
+      for (int k = 0; k < n; ++k) {
         c[i][j] += a[i][k] * b[k][j];
       }
     }
   }
 }
 
+
 int main()
 {
-  int n = 256;
   double a[n][n];
   double b[n][n];
-  double c[n][n];
-  init(a, n);
-  init(b, n);
-  multiply(a, b, c, n, n, n);
+  init(a);
+  init(b);
+  multiply(a, b, c);
   printf("the end\n");
 }
