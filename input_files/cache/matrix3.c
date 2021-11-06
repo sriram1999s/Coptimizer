e86046c6889b6c2a4074c8e4c4f96c81e5db61a1
TheCodeYoda <smurf61988@gmail.com>
2021-11-06 21:44:20 +0530

    floyd warshall's algo (there is a bug regarding closing of braces)

---
+++ input_files/cache/matrix3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void printMatrix(int nV, int matrix[][nV])
+{
+  int INF = 9999;
+  for (int i = 0; i < nV; i++) {
+    for (int j = 0; j < nV; j++) {
+      if (matrix[i][j] == INF) {
+        printf("%4s", "INF");
+      }
+      else {
+        printf("%4d", matrix[i][j]);
+      }
+    }
+    printf("\n");
+  }
+}
+
+void init(int nV, int graph[][nV], int matrix[][nV])
+{
+  for (int i = 0; i < nV; i++) {
+    for (int j = 0; j < nV; j++) {
+      matrix[i][j] = graph[i][j];
+    }
+  }
+}
+
+void floydWarshall(int nV, int graph[][nV])
+{
+  int matrix[nV][nV];
+  init(nV, graph, matrix);
+  for (int k = 0; k < nV; k++) {
+    for (int i = 0; i < nV; i++) {
+      for (int j = 0; j < nV; j++) {
+        if (matrix[i][k] + matrix[k][j] < matrix[i][j]) {
+          matrix[i][j] = matrix[i][k] + matrix[k][j];
+}
+      }
+    }
+  }
+  printMatrix(nV, matrix);
+}
+
+int main()
+{
+  int nV = 4;
+  int INF = 9999;
+  int graph[4][4] = {{0, 3, INF, 5}, {2, 0, INF, 4}, {INF, 1, 0, INF}, {INF, INF, 2, 0}};
+  floydWarshall(nV, graph);
+}
