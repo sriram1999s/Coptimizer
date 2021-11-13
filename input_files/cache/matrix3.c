bce5155280e88243ade87b8e8672a493a7ec6a69
TheCodeYoda <smurf61988@gmail.com>
2021-11-13 20:16:22 +0530

    worst case of floyd warshall's algo

--- input_files/cache/matrix3.c
+++ input_files/cache/matrix3.c
@@ -24,9 +24,9 @@ void floydWarshall(int nV, int graph[][nV])
 {
   int matrix[nV][nV];
   init(nV, graph, matrix);
-  for (int k = 0; k < nV; k++) {
+  for (int j = 0; j < nV; j++) {
     for (int i = 0; i < nV; i++) {
-      for (int j = 0; j < nV; j++) {
+      for (int k = 0; k < nV; k++) {
         if (matrix[i][k] + matrix[k][j] < matrix[i][j]) {
           matrix[i][j] = matrix[i][k] + matrix[k][j];
         }
