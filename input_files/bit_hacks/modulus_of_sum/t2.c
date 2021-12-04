8ebf8bb9bc20c7b422f3ad8894061365e8ed1b11
Khushei Meghana Meda <43701562+khusheimeda@users.noreply.github.com>
2021-12-04 23:49:17 +0530

    fixed error in test case

--- input_files/bit_hacks/modulus_of_sum/t2.c
+++ input_files/bit_hacks/modulus_of_sum/t2.c
@@ -5,17 +5,5 @@ int main()
     int r = (2+3)%5;
     printf("%d\n", r);
 
-    r = (2+ -1) % 5;
-    printf("%d\n", r);
-
-    r = (-2+ -1) % 5;
-    printf("%d\n", r);
-
-    r = (-2+ 1) % 5;
-    printf("%d\n", r);
-
-    r = (2+ 1) % -5;
-    printf("%d\n", r);
-
     return 0;
 }
